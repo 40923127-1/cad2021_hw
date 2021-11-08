2db7d1c39182d4770670c1a4699c8acee1d845a9
40923127second <allan900524@gmail.com>
2021-11-08 11:23:49 +0800

    wink1108

---
+++ downloads/123.js
@@ -0,0 +1,11 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 541, y: 316, width: 36, height: 14 }, target: 3 },
+    ],
+  },
+};
