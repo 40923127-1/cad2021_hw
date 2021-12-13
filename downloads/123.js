7e212241a179fd6a502493e9e2efdbcd4f4d9156
40923127second <allan900524@gmail.com>
2021-12-13 11:51:55 +0800

    mearm 1

--- downloads/123.js
+++ downloads/123.js
@@ -4,8 +4,5 @@
   buttonFrameLength: 5,
   buttonFrameOffset: 2,
   frameStops: {
-    "2": [
-      { type: "gotoframe", rect: { x: 541, y: 316, width: 36, height: 14 }, target: 3 },
-    ],
   },
 };
